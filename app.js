0f4953f7fc9bffbeb3720ff80b4577e9b24344bd
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2025-03-26 10:40:45 +0100

    Updated time and date of Los Angeles and Paris

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+function updateTime() {
+  // Los Angeles
+  let losAngeles = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngeles.querySelector(".date");
+  let losAngelesTime = losAngeles.querySelector(".time");
+  let losAngelesTimezone = moment().tz("America/Los_Angeles");
+  losAngelesDate.innerHTML = losAngelesTimezone.format("MMMM Do YYYY");
+  losAngelesTime.innerHTML = losAngelesTimezone.format("h:mm:ss");
 
+  // Paris
+  let paris = document.querySelector("#paris");
+  let parisDate = paris.querySelector(".date");
+  let parisTime = paris.querySelector(".time");
+  let parisTimezone = moment().tz("Europe/Paris");
+  parisDate.innerHTML = parisTimezone.format("MMMM Do YYYY");
+  parisTime.innerHTML = parisTimezone.format("h:mm:ss");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
