153040ba1edcb32a350810e7e36c2f242492c365
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2025-03-27 11:02:59 +0100

    Fixed some issues

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 function updateTime() {
   // Los Angeles
   let losAngeles = document.querySelector("#los-angeles");
-  let losAngelesDate = losAngeles.querySelector(".date");
-  let losAngelesTime = losAngeles.querySelector(".time");
-  let losAngelesTimezone = moment().tz("America/Los_Angeles");
-  losAngelesDate.innerHTML = losAngelesTimezone.format("MMMM Do YYYY");
-  losAngelesTime.innerHTML = losAngelesTimezone.format("h:mm:ss");
+  if (losAngeles) {
+    let losAngelesDate = losAngeles.querySelector(".date");
+    let losAngelesTime = losAngeles.querySelector(".time");
+    let losAngelesTimezone = moment().tz("America/Los_Angeles");
+    losAngelesDate.innerHTML = losAngelesTimezone.format("MMMM Do YYYY");
+    losAngelesTime.innerHTML = losAngelesTimezone.format("h:mm:ss");
+  }
 
   // Paris
   let paris = document.querySelector("#paris");
-  let parisDate = paris.querySelector(".date");
-  let parisTime = paris.querySelector(".time");
-  let parisTimezone = moment().tz("Europe/Paris");
-  parisDate.innerHTML = parisTimezone.format("MMMM Do YYYY");
-  parisTime.innerHTML = parisTimezone.format("h:mm:ss");
+  if (paris) {
+    let parisDate = paris.querySelector(".date");
+    let parisTime = paris.querySelector(".time");
+    let parisTimezone = moment().tz("Europe/Paris");
+    parisDate.innerHTML = parisTimezone.format("MMMM Do YYYY");
+    parisTime.innerHTML = parisTimezone.format("h:mm:ss");
+  }
 
   // Montreal
   let montreal = document.querySelector("#montreal");
-  let montrealDate = montreal.querySelector(".date");
-  let montrealTime = montreal.querySelector(".time");
-  let montrealTimezone = moment().tz("America/Montreal");
-  montrealDate.innerHTML = montrealTimezone.format("MMMM Do YYYY");
-  montrealTime.innerHTML = montrealTimezone.format("h:mm:ss");
+  if (montreal) {
+    let montrealDate = montreal.querySelector(".date");
+    let montrealTime = montreal.querySelector(".time");
+    let montrealTimezone = moment().tz("America/Montreal");
+    montrealDate.innerHTML = montrealTimezone.format("MMMM Do YYYY");
+    montrealTime.innerHTML = montrealTimezone.format("h:mm:ss");
+  }
 }
 
 updateTime();
